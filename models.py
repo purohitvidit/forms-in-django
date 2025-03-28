58ffbbc884b5083c040c2f156bf759c4cbd6c27f
purohitvidit <purohitvidit123@gmail.com>
2025-03-28 11:34:49 +0000

    Update models.py

--- models.py
+++ models.py
@@ -2,3 +2,39 @@ from django.db import models
 
 # Create your models here.
 
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # Link to Django User
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.firstname} {self.lastname}'
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='products/')
+
+    def __str__(self):
+        return self.name
+    
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
